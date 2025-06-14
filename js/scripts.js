0aca1b1dede22d848495c27754fc28f50a4f2ac4
Maaloul Khalil <maaloul.mohamedkhalil@gmail.com>
2025-06-14 21:16:18 +0100

    date fix2

--- js/scripts.js
+++ js/scripts.js
@@ -281,7 +281,7 @@ function getFormattedDate() {
   const hours = pad(now.getHours());
   const minutes = pad(now.getMinutes());
   
-  return `${month}/${day} at ${hours}:${minutes}`;
+  return `${month}/${day} - ${hours}:${minutes}`;
 }
 
 (function () {
